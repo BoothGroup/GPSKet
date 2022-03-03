f6f0708a152f7912822ef6a41196b7c61d073ada
Yannic Rath <yannic.rath@kcl.ac.uk>
2022-03-03 12:42:51 +0000

    Simplify J1-J2 setup

--- qGPSKet/operator/hamiltonian/J1J2.py
+++ qGPSKet/operator/hamiltonian/J1J2.py
@@ -12,37 +12,17 @@ from typing import Optional
 class HeisenbergOnTheFly(nk.operator.Heisenberg):
     pass
 
-def edges_2D(Lx, Ly, next_neighbours=True):
-    edges = []
-    for i in range(Ly):
-        for j in range(Lx):
-            edges.append([i * Lx + j, i * Lx + (j+1)%Lx, 0])
-            edges.append([i * Lx + j, ((i+1)%Ly) * Lx + j, 0])
-            if next_neighbours:
-                edges.append([i * Lx + j, ((i+1)%Ly) * Lx + (j+1)%Lx, 1])
-                edges.append([i * Lx + j, ((i+1)%Ly) * Lx + (j-1)%Lx, 1])
-    return edges
-
-def edges_1D(L, next_neighbours=True):
-    edges = []
-    for i in range(L):
-        edges.append([i, (i + 1)%L, 0])
-        if next_neighbours:
-            edges.append([i, (i + 2)%L, 1])
-    return edges
-
 def get_J1_J2_Hamiltonian(Lx, Ly=None, J1=1., J2=0., sign_rule=True, total_sz=0.0, on_the_fly_en=False):
     if J2 != 0.:
-        next_neighbours = True
+        nb_order = 2
     else:
-        next_neighbours = False
+        nb_order = 1
 
     if Ly is None:
-        edges = edges_1D(Lx, next_neighbours=next_neighbours)
+        g = nk.graph.Chain(Lx, max_neighbor_order=nb_order)
     else:
-        edges = edges_2D(Lx, Ly, next_neighbours=next_neighbours)
+        g = nk.graph.Grid([Lx, Ly], max_neighbor_order=nb_order)
 
-    g = nk.graph.Graph(edges=edges)
     hilbert = nk.hilbert.Spin(0.5, total_sz=total_sz, N=g.n_nodes)
 
     """
