94509c3ab4a25d1b2f0fd8fbf340855dabc40899
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2022-03-11 16:48:15 +0000

    Update init

--- qGPSKet/sampler/__init__.py
+++ qGPSKet/sampler/__init__.py
@@ -1,2 +1,4 @@
 from .metropolis_fast import MetropolisFastExchange
 from .autoreg import ARDirectSampler
+from .fermionic_hopping import MetropolisHopping
+from .fermionic_hopping import MetropolisFastHopping
