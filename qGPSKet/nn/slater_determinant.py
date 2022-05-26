30ce7ed7ab40e19e99766fa874a356a2bb656755
Yannic Rath <yannic.rath@kcl.ac.uk>
2022-05-26 13:05:47 +0100

    Add symmetrization interface SD

--- qGPSKet/nn/slater_determinant.py
+++ qGPSKet/nn/slater_determinant.py
@@ -4,6 +4,7 @@ from flax import linen as nn
 from typing import Tuple
 from netket.utils.types import Array, Callable, DType
 from qGPSKet.nn import normal
+import jax
 
 
 class SlaterDeterminant(nn.Module):
@@ -11,6 +12,12 @@ class SlaterDeterminant(nn.Module):
     n_elec: Tuple[int, int]
     dtype: DType = jnp.complex128
     init_fun: Callable = normal()
+    symmetries: Callable = lambda inputs : jnp.expand_dims(inputs, axis=-1)
+    out_transformation: Callable = lambda x: jax.scipy.special.logsumexp(x, axis=-1)
+    """ The following flag determines whether the S^2 symmetry (with S=0) should be enforced
+    by using the same orbitals for up and down spin.
+    """
+    spin_symmetry_by_structure: bool = False
 
     @nn.compact
     def __call__(self, x) -> Array:
@@ -21,16 +28,23 @@ class SlaterDeterminant(nn.Module):
         # Initialize U matrix, which has block structure in terms of
         # up and down electrons
         U_up = self.param("U_up", self.init_fun, (self.n_sites, self.n_elec[0]), self.dtype)
-        U_down = self.param("U_down", self.init_fun, (self.n_sites, self.n_elec[1]), self.dtype)
+        if self.spin_symmetry_by_structure:
+            U_down = U_up
+        else:
+            U_down = self.param("U_down", self.init_fun, (self.n_sites, self.n_elec[1]), self.dtype)
 
-        # Compute Slater determinant as product of the determinants of the
-        # matrices Ũ_up and Ũ_down, which have rows from U_up and U_down 
-        # corresponding to electron positions in y:
-        # SD = det(Ũ_up)det(Ũ_down)
-        Ũ_up = jnp.take(U_up, x[:, :self.n_elec[0]], axis=0)
-        Ũ_down = jnp.take(U_down, x[:, self.n_elec[0]:], axis=0)
-        (s_up, log_det_up) = jnp.linalg.slogdet(Ũ_up)
-        (s_down, log_det_down) = jnp.linalg.slogdet(Ũ_down)
-        log_sd = log_det_up + log_det_down + jnp.log(s_up*s_down+0j)
+        y = self.symmetries(x)
 
-        return log_sd
+        def evaluate_SD(y_sym):
+            # Compute Slater determinant as product of the determinants of the
+            # matrices Ũ_up and Ũ_down, which have rows from U_up and U_down
+            # corresponding to electron positions in y:
+            # SD = det(Ũ_up)det(Ũ_down)
+            Ũ_up = jnp.take(U_up, y_sym[:, :self.n_elec[0]], axis=0)
+            Ũ_down = jnp.take(U_down, y_sym[:, self.n_elec[0]:], axis=0)
+            (s_up, log_det_up) = jnp.linalg.slogdet(Ũ_up)
+            (s_down, log_det_down) = jnp.linalg.slogdet(Ũ_down)
+            return log_det_up + log_det_down + jnp.log(s_up*s_down+0j)
+        value = jax.vmap(evaluate_SD, in_axes=-1, out_axes=-1)(y)
+
+        return self.out_transformation(value)
