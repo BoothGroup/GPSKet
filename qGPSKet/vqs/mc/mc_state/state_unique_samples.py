ed5dbcfadf397ce497494bd4dbd4c700d02c8cdb
Yannic Rath <yannic.rath@kcl.ac.uk>
2022-06-27 12:11:29 +0100

    Fix and imporve unique samples state

--- qGPSKet/vqs/mc/mc_state/state_unique_samples.py
+++ qGPSKet/vqs/mc/mc_state/state_unique_samples.py
@@ -1,14 +1,33 @@
 import netket as nk
 import netket.jax as nkjax
 import jax.numpy as jnp
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Callable, Any
 from collections import defaultdict
 import numpy as np
 
+from netket.utils.types import PyTree
+
+from netket.utils.dispatch import TrueT
+
 from netket.utils.mpi import (
     node_number as _rank,
-    mpi_sum as _mpi_sum
+    mpi_sum as _mpi_sum,
+    n_nodes as _n_nodes,
+    mpi_sum_jax as _mpi_sum_jax
 )
+
+from netket.stats import Stats
+
+from netket.stats.mpi_stats import (
+    mean as _mean,
+    sum as _sum
+)
+
+from functools import partial
+
+from netket.vqs import get_local_kernel_arguments, get_local_kernel
+
+import jax
 
 """ Very hacky implementation of an MC state which samples until it has accumulated n_samples
 unique samples. Expectation values then also include the number of times each of the unique samples
@@ -19,6 +38,10 @@ of expectation values can be trusted and sampler properties and variances are pr
 approach is just hacked into the code with the least possible overhead.
 """
 class MCStateUniqeSamples(nk.vqs.MCState):
+    def __init__(self, *args, max_sampling_steps=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.max_sampling_steps = max_sampling_steps
+
     def reset(self):
         self._samples = None
         self._unique_samples = None
@@ -38,8 +61,9 @@ class MCStateUniqeSamples(nk.vqs.MCState):
             end_id = start_id + self.n_samples_per_rank
 
             unique_samps = defaultdict(lambda: 0)
+            continue_sampling = True
             count = 0
-            while(len(unique_samps) < self.n_samples):
+            while(len(unique_samps) < self.n_samples and continue_sampling):
                 if count == 0:
                     samps = self.sample()
                 else:
@@ -64,39 +88,73 @@ class MCStateUniqeSamples(nk.vqs.MCState):
                     unique_samps[tuple(samp)] += 1
                     if len(unique_samps) == self.n_samples:
                         break
-            unique_samples = np.array(list(unique_samps.keys()), dtype=samps.dtype)
-            relative_counts = np.array(list(unique_samps.values()), dtype=float)
+                count += 1
 
-            """
-            Required so that we can reuse all the standard code to evaluate expectation values.
-            """
-            relative_counts *= self.n_samples/np.sum(relative_counts)
+                if self.max_sampling_steps is not None:
+                    if self.max_sampling_steps <= count:
+                        continue_sampling = False
+
+            unique_samples = all_samples
+            relative_counts = np.zeros(all_samples.shape[0])
+            np.copyto(unique_samples[:len(unique_samps), :], np.array(list(unique_samps.keys()), dtype=samps.dtype))
+            np.copyto(relative_counts[:len(unique_samps)], np.array(list(unique_samps.values()), dtype=float))
 
             # Split samples and counts across mpi processes
-            self._unique_samples = jnp.array(unique_samples[start_id:end_id, :])
+            self._unique_samples = jnp.array(all_samples[start_id:end_id, :])
             self._relative_counts = jnp.array(relative_counts[start_id:end_id])
 
         return (self._unique_samples, self._relative_counts)
 
 
-""" Below code is a very hacky way to inject the relative counts into the standard
-evaluation of expectation values """
-def count_wrapped(inner_fun, logpsi, pars, samples, args, *add_args, **kwargs):
-    return args[-1].reshape(-1) * inner_fun(logpsi, pars, samples, args[:-1], *add_args, **kwargs)
+""" The following functions just override the NetKet implementation to inject the sample counts into the expectation value evaluation."""
+@nk.vqs.expect_and_grad.dispatch(precedence=10)
+def expect_and_grad(vstate: MCStateUniqeSamples, op: nk.operator.AbstractOperator, use_covariance: TrueT, chunk_size: Optional[int], *, mutable:Any):
+    _, args = get_local_kernel_arguments(vstate, op)
+    samples_and_counts = vstate.samples
+    local_estimator = get_local_kernel(vstate, op, chunk_size)
+    assert(mutable is False)
 
-@nk.vqs.get_local_kernel.dispatch(precedence=10)
-def get_local_kernel(vstate: MCStateUniqeSamples, op: nk.operator.AbstractOperator, chunk_size: Optional[int] = None):
-    vstate.__class__ = nk.vqs.MCState
-    inner_fun = get_local_kernel(vstate, op, chunk_size=chunk_size)
-    vstate.__class__ = MCStateUniqeSamples
-    return nkjax.HashablePartial(count_wrapped, inner_fun)
+    exp, grad = grad_expect_hermitian_chunked(chunk_size, local_estimator, vstate._apply_fun, vstate.parameters, vstate.model_state, samples_and_counts, args, compute_grad=True)
 
+    return exp, grad
 
-@nk.vqs.get_local_kernel_arguments.dispatch(precedence=10)
-def get_local_kernel_arguments(vstate: MCStateUniqeSamples, op: nk.operator.AbstractOperator):
-    samples = vstate.samples[0]
-    vstate.__class__ = nk.vqs.MCState
-    args = get_local_kernel_arguments(vstate, op)[1]
-    vstate.__class__ = MCStateUniqeSamples
-    args_with_counts = (*args, vstate.samples[1])
-    return (samples, args_with_counts)
+@nk.vqs.expect.dispatch(precedence=10)
+def expect(vstate: MCStateUniqeSamples, op: nk.operator.AbstractOperator, chunk_size: Optional[int]):
+    _, args = get_local_kernel_arguments(vstate, op)
+    samples_and_counts = vstate.samples
+    local_estimator = get_local_kernel(vstate, op, chunk_size)
+
+    exp = grad_expect_hermitian_chunked(chunk_size, local_estimator, vstate._apply_fun, vstate.parameters, vstate.model_state, samples_and_counts, args, compute_grad=False)
+
+    return exp
+
+@partial(jax.jit, static_argnums=(0,1,2,7))
+def grad_expect_hermitian_chunked(chunk_size: int, estimator_fun: Callable, model_apply_fun: Callable, parameters: PyTree, model_state: PyTree, samples_and_counts: Tuple[jnp.ndarray, jnp.ndarray], estimator_args: PyTree, compute_grad=False):
+    samples = samples_and_counts[0]
+    counts = samples_and_counts[1]
+
+    n_samples = _sum(counts)
+
+    loc_vals = estimator_fun(model_apply_fun, {"params": parameters, **model_state}, samples, estimator_args, chunk_size=chunk_size)
+
+    mean = _mean(counts * loc_vals)
+
+    variance = _sum(counts * (jnp.abs(loc_vals - mean)**2))/n_samples
+
+    loc_val_stats = Stats(mean=mean, variance=variance)
+
+    if compute_grad:
+        loc_vals_centered = counts * (loc_vals - loc_val_stats.mean)
+
+        if chunk_size is not None:
+            vjp_fun = nkjax.vjp_chunked(lambda w, samps: model_apply_fun({"params": w, **model_state}, samps), parameters, samples, conjugate=True, chunk_size=chunk_size, chunk_argnums=1, nondiff_argnums=1)
+        else:
+            vjp_fun = nkjax.vjp(lambda w, samps: model_apply_fun({"params": w, **model_state}, samps), parameters, samples, conjugate=True)[1]
+
+        val_grad = vjp_fun((jnp.conjugate(loc_vals_centered) / n_samples))[0]
+
+        val_grad = jax.tree_map(lambda x, target: (x if jnp.iscomplexobj(target) else 2 * x.real).astype(target.dtype), val_grad, parameters)
+
+        return loc_val_stats, jax.tree_map(lambda x: _mpi_sum_jax(x)[0], val_grad)
+    else:
+        return loc_val_stats
