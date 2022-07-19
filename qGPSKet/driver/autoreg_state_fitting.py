3c9e0cbd908cf64b583700818e369bf42a91e9ae
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2022-07-19 18:36:33 +0100

    Improve sampling

--- qGPSKet/driver/autoreg_state_fitting.py
+++ qGPSKet/driver/autoreg_state_fitting.py
@@ -1,6 +1,10 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 from functools import partial
+from typing import Tuple, Optional
+from netket.utils.types import Array, SeedT
+from netket.operator import AbstractOperator
 from netket.utils import mpi
 from netket.driver.vmc_common import info
 from .abstract_state_fitting import AbstractStateFittingDriver
@@ -11,17 +15,32 @@ class ARStateFitting(AbstractStateFittingDriver):
     Fit an autoregressive Ansatz to data from another state by minimizing the distance between two normalized quantum states
     """
 
-    def __post_init__(self):
-        if not hasattr(self._variational_state.model, 'conditional'):
+    def __init__(
+        self,
+        dataset: Tuple[Array, Array],
+        hamiltonian: AbstractOperator,
+        optimizer,
+        *args,
+        variational_state=None,
+        mini_batch_size: int = 32,
+        seed: Optional[SeedT]=None,
+        **kwargs
+    ):
+        super().__init__(dataset, hamiltonian, optimizer, *args, variational_state=variational_state, mini_batch_size=mini_batch_size, seed=seed, **kwargs)
+
+        if not hasattr(self._variational_state.model, 'conditionals'):
             raise ValueError(
                 f"{self._variational_state.model} is not autoregressive."
             )
+
+        self._probas = np.abs(self._dataset[1])**2
+        self._ids = np.arange(self._size_dataset)
         
 
     def _forward_and_backward(self):
         # Sample mini-batch
         self._key, _ = jax.random.split(self._key)
-        mini_batch_ids = jax.random.randint(self._key, (self._mini_batch_size,), minval=0, maxval=self._size_dataset)
+        mini_batch_ids = jax.random.choice(self._key, self._ids, (self._mini_batch_size,), p=self._probas, replace=False)
         mini_batch = (self._dataset[0][mini_batch_ids], self._dataset[1][mini_batch_ids])
 
         # Compute loss and gradient
