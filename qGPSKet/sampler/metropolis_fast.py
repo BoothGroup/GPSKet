a32b24c69a2fc9ac25c11bce53db5fa5cac0c074
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2022-09-09 11:29:11 +0100

    Update loops API

--- qGPSKet/sampler/metropolis_fast.py
+++ qGPSKet/sampler/metropolis_fast.py
@@ -1,6 +1,5 @@
 import jax
 import jax.numpy as jnp
-from jax.experimental import loops
 from netket.utils import struct
 from netket.sampler.metropolis import MetropolisSampler, MetropolisRule
 from netket.sampler.rules.exchange import compute_clusters
@@ -27,61 +26,64 @@ class MetropolisFastSampler(MetropolisSampler):
         (together with intermediate values) are copied at each sampling step. However there is less
         overhead as the amplitude computation is performed by fast updating.
         """
-        new_rng, rng = jax.random.split(state.rng)
 
-        with loops.Scope() as s:
-            s.key = rng
-            s.σ = state.σ
-            value, s.intermediates_cache = machine.apply(parameters, state.σ, mutable="intermediates_cache", cache_intermediates=True)
-            s.log_prob = sampler.machine_pow * value.real
+        def loop_body(i, s):
+            # 1 to propagate for next iteration, 1 for uniform rng and n_chains for transition kernel
+            s["key"], key1, key2 = jax.random.split(s["key"], 3)
 
-            # for logging
-            s.accepted = state.n_accepted_proc
-
-            for i in s.range(sampler.n_sweeps):
-                # 1 to propagate for next iteration, 1 for uniform rng and n_chains for transition kernel
-                s.key, key1, key2 = jax.random.split(s.key, 3)
-
-                σp, log_prob_correction, update_sites = sampler.rule.transition(
-                    sampler, machine, parameters, state, key1, s.σ
-                )
-
-                params = {**parameters, **s.intermediates_cache}
-                updated_occupancy = jax.vmap(jnp.take, in_axes=(0, 0), out_axes=0)(σp, update_sites)
-                value, new_intermediates_cache = machine.apply(params, updated_occupancy, mutable="intermediates_cache", cache_intermediates=True, update_sites=update_sites)
-                proposal_log_prob = (
-                    sampler.machine_pow * value.real
-                )
-
-                uniform = jax.random.uniform(key2, shape=(sampler.n_chains_per_rank,))
-                if log_prob_correction is not None:
-                    do_accept = uniform < jnp.exp(
-                        proposal_log_prob - s.log_prob + log_prob_correction
-                    )
-                else:
-                    do_accept = uniform < jnp.exp(proposal_log_prob - s.log_prob)
-
-                # do_accept must match ndim of proposal and state (which is 2)
-                s.σ = jnp.where(do_accept.reshape(-1, 1), σp, s.σ)
-
-                def update(old_state, new_state):
-                    return jax.vmap(jnp.where)(do_accept, old_state, new_state)
-
-                s.intermediates_cache = jax.tree_map(update, new_intermediates_cache, s.intermediates_cache)
-
-                s.accepted += do_accept.sum()
-
-                s.log_prob = jax.numpy.where(
-                    do_accept.reshape(-1), proposal_log_prob, s.log_prob
-                )
-
-            new_state = state.replace(
-                rng=new_rng,
-                σ=s.σ,
-                n_accepted_proc=s.accepted,
-                n_steps_proc=state.n_steps_proc
-                + sampler.n_sweeps * sampler.n_chains_per_rank,
+            σp, log_prob_correction, update_sites = sampler.rule.transition(
+                sampler, machine, parameters, state, key1, s["σ"]
             )
+
+            params = {**parameters, **s["intermediates_cache"]}
+            updated_occupancy = jax.vmap(jnp.take, in_axes=(0, 0), out_axes=0)(σp, update_sites)
+            value, new_intermediates_cache = machine.apply(params, updated_occupancy, mutable="intermediates_cache", cache_intermediates=True, update_sites=update_sites)
+            proposal_log_prob = (
+                sampler.machine_pow * value.real
+            )
+
+            uniform = jax.random.uniform(key2, shape=(sampler.n_chains_per_rank,))
+            if log_prob_correction is not None:
+                do_accept = uniform < jnp.exp(
+                    proposal_log_prob - s["log_prob"] + log_prob_correction
+                )
+            else:
+                do_accept = uniform < jnp.exp(proposal_log_prob - s["log_prob"])
+
+            # do_accept must match ndim of proposal and state (which is 2)
+            s["σ"] = jnp.where(do_accept.reshape(-1, 1), σp, s["σ"])
+
+            def update(old_state, new_state):
+                return jax.vmap(jnp.where)(do_accept, old_state, new_state)
+
+            s["intermediates_cache"] = jax.tree_map(update, new_intermediates_cache, s["intermediates_cache"])
+
+            s["accepted"] += do_accept.sum()
+
+            s["log_prob"] = jax.numpy.where(
+                do_accept.reshape(-1), proposal_log_prob, s["log_prob"]
+            )
+
+            return s
+
+        new_rng, rng = jax.random.split(state.rng)
+        value, intermediates_cache = machine.apply(parameters, state.σ, mutable="intermediates_cache", cache_intermediates=True)
+        init_s = {
+            "key": rng,
+            "σ": state.σ,
+            "intermediates_cache": intermediates_cache,
+            "log_prob": sampler.machine_pow*value.real,
+            "accepted": state.n_accepted_proc
+        }
+        s = jax.lax.fori_loop(0, sampler.n_Sweeps, loop_body, init_s)
+
+        new_state = state.replace(
+            rng=new_rng,
+            σ=s["σ"],
+            n_accepted_proc=s["accepted"],
+            n_steps_proc=state.n_steps_proc
+            + sampler.n_sweeps * sampler.n_chains_per_rank,
+        )
 
         return new_state, new_state.σ
     def dummy(self):
