72e61e56e3f9db5ff3be7ee121d7968bc5339b88
Yannic Rath <yannic.rath@kcl.ac.uk>
2022-09-12 14:35:46 +0100

    Use local estimators interface for imag time step

--- qGPSKet/supervised/imag_time_step.py
+++ qGPSKet/supervised/imag_time_step.py
@@ -6,12 +6,6 @@ from functools import partial
 
 class ImagTimeStep():
     def __init__(self, vstate, hamiltonian):
-        loc_kernel = partial(nk.vqs.get_local_kernel(vstate, hamiltonian), vstate._apply_fun)
-        def loc_en(samples, parameters, args):
-            if len(samples.shape) != 2:
-                samples = samples.reshape((-1, samples.shape[-1]))
-            return loc_kernel({"params": parameters}, samples, args)
-        self.loc_ens = jax.jit(loc_en)
         self.vstate = vstate
         self.hamiltonian = hamiltonian
 
@@ -19,8 +13,7 @@ class ImagTimeStep():
         # This is a little bit hacky, the interface should probably be improved at one point
         old_samples = self.vstate._samples
         self.vstate._samples = samples
-        _, args = nk.vqs.get_local_kernel_arguments(self.vstate, self.hamiltonian)
-        loc_ens = self.loc_ens(samples, self.vstate.parameters, args)
+        loc_ens = self.vstate.local_estimators(self.hamiltonian, chunk_size=self.vstate.chunk_size)
         self.vstate._samples = old_samples
         return loc_ens
 
