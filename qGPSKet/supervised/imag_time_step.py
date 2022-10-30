033c08b27b21a70e572e68242e1d9eebe7de7a36
Yannic Rath <yannic.rath@kcl.ac.uk>
2022-10-30 19:21:50 +0000

    Add imaginary time step variational state

--- qGPSKet/supervised/imag_time_step.py
+++ qGPSKet/supervised/imag_time_step.py
@@ -1,6 +1,9 @@
 import netket as nk
 import jax
 import jax.numpy as jnp
+import copy
+
+from netket.vqs.mc import get_local_kernel_arguments, get_local_kernel
 
 from functools import partial
 
@@ -22,3 +25,26 @@ class ImagTimeStep():
         self.log_amps = self.vstate.log_value(samples_reshaped)
         self.local_energies = self.get_local_energies(samples_reshaped)
         return self.log_amps + jnp.log(1 - tau * self.local_energies)
+
+def get_imag_time_step_vstate(tau, hamiltonian, vstate):
+    """Returns a variational state with a first order imaginary time evolved model (i.e. (1 - tau H)|Psi>)
+    based on a given variational state, can therefore be nested arbitrarily often.
+
+    Args:
+        tau: Propagation time
+        hamiltonian: The Hamiltonian for the imaginary time evolution
+        vstate: The original variational state
+
+    Returns:
+        The variational state with updated model (based on a single imaginary time step)
+    """
+    log_model = vstate._apply_fun
+    _, args = get_local_kernel_arguments(vstate, hamiltonian)
+    local_estimator_fun = get_local_kernel(vstate, hamiltonian, chunk_size=vstate.chunk_size)
+    def imag_time_model_log_amp(model_pars, samples):
+        loc_ens = local_estimator_fun(log_model, model_pars, samples, args)
+        log_amps = log_model(model_pars, samples)
+        return log_amps + jnp.log(1 - tau * loc_ens)
+    new_vstate = copy.deepcopy(vstate)
+    new_vstate._apply_fun = imag_time_model_log_amp
+    return new_vstate
