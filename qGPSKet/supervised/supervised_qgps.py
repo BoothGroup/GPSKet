Fix broadcasting of weights

qGPSKet/supervised/supervised_qgps.py
@@ -233,18 +233,15 @@ class QGPSLearning():
                 weights = sp.linalg.cho_solve((L, True), self.y[self.active_elements])
                 self.Sinv = None
                 self.cholesky = True
+                # This is just to ensure that each rank does the same update
+                _MPI_comm.Bcast(weights)
+                _MPI_comm.Bcast(self.Sinv_L)
             except:
                 self.Sinv = sp.linalg.pinvh(self.KtK_alpha[np.ix_(self.active_elements, self.active_elements)])
                 weights = self.Sinv.dot(self.y[self.active_elements])
                 self.cholesky = False
-
-        # This is just to ensure that each rank does the same update
-        _MPI_comm.Bcast(weights)
-
-        if self.cholesky:
-            _MPI_comm.Bcast(self.Sinv_L)
-        else:
-            _MPI_comm.Bcast(self.Sinv)
+                _MPI_comm.Bcast(weights)
+                _MPI_comm.Bcast(self.Sinv)
 
         if self.weights is None:
             if not self.complex_expand and self.epsilon.dtype==complex: