6206f1c99f685f3cbc18bf0020d81a358fc97188
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2023-08-03 15:22:28 +0100

    Implement chunking

--- GPSKet/driver/minSR.py
+++ GPSKet/driver/minSR.py
@@ -9,49 +9,120 @@ import netket as nk
 from netket import VMC
 from netket.stats import Stats
 from netket.utils import mpi
+from netket.vqs.mc.mc_state.state import check_chunk_size
 from netket.stats._autocorr import integrated_time
 from netket.stats.mc_stats import _split_R_hat
 
-from GPSKet.vqs import MCStateUniqueSamples
 
 class minSRVMC(VMC):
     """
     VMC driver utilizing the minSR updates as proposed in https://arxiv.org/abs/2302.01941
     """
     def __init__(self, *args, mode: str = None, holomorphic: bool = None,
-                 solver=lambda A, b: jnp.linalg.lstsq(A, b, rcond=1.e-12)[0], diag_shift: float = 0., **kwargs):
+                 solver=lambda A, b: jnp.linalg.lstsq(A, b, rcond=1.e-12)[0], **kwargs):
         super().__init__(*args, **kwargs)
         assert(not (mode is not None and holomorphic is not None))
-        assert (diag_shift >= 0.) and (diag_shift <= 1.)
         if mode is None:
-            self.mode = nk.jax.jacobian_default_mode(self.state._apply_fun, self.state.parameters,
-                                             self.state.model_state, self.state.samples,
-                                             holomorphic=holomorphic)
+            self.mode = nk.jax.jacobian_default_mode(
+                self.state._apply_fun,
+                self.state.parameters,
+                self.state.model_state,
+                self.state.samples,
+                holomorphic=holomorphic
+            )
         else:
             self.mode = mode
         self.solver = solver
-        self.diag_shift = diag_shift
+        if self.state.chunk_size is None:
+            self.state.chunk_size = self.state.n_samples
+        check_chunk_size(self.state.n_samples, self.state.chunk_size)
 
     # Super simple implementation of the minSR driver
     def _forward_and_backward(self):
         self.state.reset()
 
+        # Gather samples and counts
         if hasattr(self.state, "samples_with_counts"):
             samples, counts = self.state.samples_with_counts
         else:
             samples = self.state.samples
             counts = jnp.ones(samples.shape[:-1])/(mpi.mpi_sum_jax(np.prod(samples.shape[:-1]))[0])
-
         samples = samples.reshape((-1, samples.shape[-1]))
         counts = counts.reshape((-1,))
 
-        # Transpose as local_estimators function flips the axes
+        # Gather samples and counts
+        if hasattr(self.state, "samples_with_counts"):
+            samples, counts = self.state.samples_with_counts
+        else:
+            samples = self.state.samples
+            counts = jnp.ones(samples.shape[:-1])/(mpi.mpi_sum_jax(np.prod(samples.shape[:-1]))[0])
+        samples = samples.reshape((-1, samples.shape[-1]))
+        counts = counts.reshape((-1,))
+
+        # Compute local energies and center them
         loc_ens = self.state.local_estimators(self._ham).T.reshape(-1)
+        self._loss_stats = _statistics(loc_ens, counts)
+        loc_ens_centered = (loc_ens-self._loss_stats.mean)*jnp.sqrt(counts)
+        loc_ens_centered = (mpi.mpi_allgather_jax(loc_ens_centered)[0]).reshape(-1)
 
-        O = nk.jax.jacobian(self.state._apply_fun, self.state.parameters, samples,
-                            self.state.model_state, mode = self.mode, pdf = counts, dense=True, center=True)
+        # Prepare chunks
+        n_samples_per_rank = samples.shape[0]
+        n_chunks = n_samples_per_rank//self.state.chunk_size
+        idx = jnp.arange(n_samples_per_rank).reshape((n_chunks, self.state.chunk_size))
+        samples = jnp.reshape(samples, (n_chunks, self.state.chunk_size, samples.shape[-1]))
+        counts = jnp.reshape(counts, (n_chunks, self.state.chunk_size))
+        loc_ens_centered = jnp.reshape(loc_ens_centered, (n_chunks, self.state.chunk_size))
 
-        self._loss_stats, self._loss_grad, dense_update = compute_update(loc_ens, O, counts, self.solver, self.diag_shift)
+        # Initialize neural tangent kernel, gradient of loss and dense update
+        dtype = jnp.complex128 if self.mode == "holomorphic" else jnp.float64
+        O_avg = jnp.zeros(self.state.n_parameters, dtype=dtype)
+        if self.mode == "holomorphic" or self.mode == "real":
+            OO = jnp.zeros((self.state.n_samples, self.state.n_samples), dtype=dtype)
+            loss_grad = jnp.zeros(self.state.n_parameters, dtype=dtype)
+            dense_update = jnp.zeros(self.state.n_parameters, dtype=jnp.complex128)
+        else:
+            OO = jnp.zeros((2*self.state.n_samples, 2*self.state.n_samples), dtype=dtype)
+            loss_grad = jnp.zeros(2*self.state.n_parameters, dtype=dtype)
+            dense_update = jnp.zeros(2*self.state.n_parameters, dtype=jnp.complex128)
+
+        # Compute gradient of loss and average of log-derivatives
+        for i in range(n_chunks):
+            counts_i = counts[i]
+            O_i = nk.jax.jacobian(self.state._apply_fun, self.state.parameters, samples[i],
+                                  self.state.model_state, mode=self.mode, pdf=counts_i, dense=True, center=False)
+            loss_grad += jnp.dot(O_i.T, loc_ens_centered[i]).real
+            O_avg += np.sum(O_i*counts_i[:, np.newaxis], axis=0)
+        
+        # Compute neural tangent kernel
+        for i in range(n_chunks):
+            idx_i = idx[i]
+            O_i = nk.jax.jacobian(self.state._apply_fun, self.state.parameters, samples[i],
+                                  self.state.model_state, mode=self.mode, pdf=counts[i], dense=True, center=False)
+            O_i = (mpi.mpi_allgather_jax(O_i)[0]).reshape((-1, *O_i.shape[1:]))
+            if len(O_i.shape) == 3:
+                O_i = O_i[:,0,:] + 1.j * O_i[:,1,:]
+            O_i = O_i-O_avg
+            for j in range(n_chunks):
+                O_j = nk.jax.jacobian(self.state._apply_fun, self.state.parameters, samples[j],
+                                      self.state.model_state, mode=self.mode, pdf=counts[j], dense=True, center=False)
+                O_j = (mpi.mpi_allgather_jax(O_j)[0]).reshape((-1, *O_j.shape[1:]))
+                if len(O_j.shape) == 3:
+                    O_j = O_j[:,0,:] + 1.j * O_j[:,1,:]
+                O_j = O_j-O_avg
+                ii, jj = np.meshgrid(idx_i, idx[j], indexing="ij")
+                OO = OO.at[ii, jj].set(O_i.dot(O_j.conj().T))
+
+        # Solve linear system and compute parameters update
+        loc_ens_centered = jnp.reshape(loc_ens_centered, (n_chunks*self.state.chunk_size,))
+        OO_epsilon = self.solver(OO, loc_ens_centered)
+        for i in range(n_chunks):
+            O_i = nk.jax.jacobian(self.state._apply_fun, self.state.parameters, samples[i],
+                                  self.state.model_state, mode=self.mode, pdf=counts[i], dense=True, center=False)
+            O_i = (mpi.mpi_allgather_jax(O_i)[0]).reshape((-1, *O_i.shape[1:]))
+            if len(O_i.shape) == 3:
+                O_i = O_i[:,0,:] + 1.j * O_i[:,1,:]
+            O_i = O_i-O_avg
+            dense_update += O_i.conj().T.dot(OO_epsilon[idx[i]])               
 
         # Convert back to pytree
         unravel = lambda x : x
@@ -62,36 +133,12 @@ class minSRVMC(VMC):
         _, unravel = nk.jax.tree_ravel(x)
 
         self._dp = reassemble(unravel(dense_update))
+        self._loss_grad = reassemble(unravel(loss_grad))
 
         # Cast to real if necessary
         self._dp = jax.tree_map(lambda x, target: (x if jnp.iscomplexobj(x) else x.real), self._dp, self.state.parameters)
 
         return self._dp
-
-
-@partial(jax.jit, static_argnames=("solver"))
-def compute_update(loc_ens, O, counts, solver, diag_shift):
-    loss_stats = _statistics(loc_ens, counts)
-
-    loc_ens_centered = (loc_ens - loss_stats.mean) * jnp.sqrt(counts)
-
-    loc_ens_centered = (mpi.mpi_allgather_jax(loc_ens_centered)[0]).reshape(-1)
-
-    O = (mpi.mpi_allgather_jax(O)[0]).reshape((-1, *O.shape[1:]))
-
-    # Complex real split, is this correct? TODO: double check
-    if len(O.shape) == 3:
-        O = O[:,0,:] + 1.j * O[:,1,:]
-
-    loss_grad = jnp.dot(O.T, loc_ens_centered).real
-
-    OO = (1-diag_shift) * O.dot(O.conj().T) + diag_shift * jnp.eye(O.shape[0])
-
-    OO_epsilon = solver(OO, loc_ens_centered)
-
-    dense_update = O.conj().T.dot(OO_epsilon)
-
-    return loss_stats, loss_grad, dense_update
 
 @jax.jit
 def _statistics(data, counts):
