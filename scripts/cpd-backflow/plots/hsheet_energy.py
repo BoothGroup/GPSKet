501f08a0bcd7c2505f37c4a65428ec88831456a9
Massimo Bortone <m.bortone@instadeep.com>
2025-03-01 12:56:27 +0000

    Refactor energy data loading to use a single CSV file

--- scripts/cpd-backflow/plots/hsheet_energy.py
+++ scripts/cpd-backflow/plots/hsheet_energy.py
@@ -12,11 +12,12 @@ h1_energy = mf.e_tot
 
 # Load reference and CPD backflow data
 n_atoms = 36
-rhf_energies = pd.read_csv('hsheet_rhf.csv')
-uccsd_energies = pd.read_csv('hsheet_uccsd.csv')
-dmrg_energies = pd.read_csv('hsheet_dmrg.csv')
-gpsxsd_energies = pd.read_csv('hsheet_gpsxsd.csv')
-df = pd.read_csv('hsheet_cpd_backflow.csv')
+df = pd.read_csv('hsheet.csv')
+rhf_energies = df.query('`method` == "rhf"')
+uccsd_energies = df.query('`method` == "uccsd"')
+dmrg_energies = df.query('`method` == "dmrg"')
+gpsxsd_energies = df.query('`method` == "gpsxsd"')
+cpdbf_energies = df.query('`method` == "cpd_backflow"')
 
 # Energy
 fig, axs = plt.subplots(2, 1, figsize=(9, 9), height_ratios=[0.7, 0.3], gridspec_kw={'hspace': 0.05}, sharex=True)
@@ -27,10 +28,10 @@ axs[0].plot(rhf_energies['distance'], rhf_energies['energy']/n_atoms, color="C2"
 axs[0].plot(uccsd_energies['distance'], uccsd_energies['energy']/n_atoms, color="C4", label="UCCSD")
 axs[0].plot(dmrg_energies['distance'], dmrg_energies['energy']/n_atoms, color="C3", marker='o', markersize=6, label="DMRG (M=1024)")
 axs[0].plot(gpsxsd_energies['distance'], gpsxsd_energies['energy']/n_atoms, color="0.2", linestyle=":", label=r"$\Psi^{GPS}\times\left|\Phi\right\rangle (M=72)$")
-sub_df = df.query('`exchange_cutoff`.isna() and `M` == 1', engine="python")
+sub_df = cpdbf_energies.query('`exchange_cutoff`.isna() and `M` == 1', engine="python")
 label = r"$\Psi^{CPD} (M=1)$"
 axs[0].errorbar(sub_df['distance'], sub_df['energy']/n_atoms, yerr=sub_df['errorbar']/n_atoms, color="0.2", linestyle="-", label=label)
-sub_df = df.query('`exchange_cutoff` == 5.0 and `M` == 1')
+sub_df = cpdbf_energies.query('`exchange_cutoff` == 5.0 and `M` == 1')
 label = r"$\Psi^{CPD} (M=1, K=5)$"
 axs[0].errorbar(sub_df['distance'], sub_df['energy']/n_atoms, yerr=sub_df['errorbar']/n_atoms, color="0.2", linestyle="--", label=label)
 axs[0].set_ylabel(r'Energy per atom ($E_h$)')
@@ -39,11 +40,11 @@ axs[0].legend(loc='lower right')
 
 ## Bottom panel
 dmrg_energies_values = dmrg_energies.query('`distance` != 2.5')['energy'].values
-sub_df = df.query('`distance` != 2.5 and `exchange_cutoff`.isna() and `M` == 1', engine="python")
+sub_df = cpdbf_energies.query('`distance` != 2.5 and `exchange_cutoff`.isna() and `M` == 1', engine="python")
 label = r"$\Psi^{CPD} (M=1)$"
 error_wrt_dmrg = (sub_df['energy']-dmrg_energies_values)/n_atoms
 axs[1].plot(sub_df['distance'], error_wrt_dmrg, color="0.2", linestyle="-", label=label)
-sub_df = df.query('`distance` != 2.5 and `exchange_cutoff` == 5.0 and `M` == 1')
+sub_df = cpdbf_energies.query('`distance` != 2.5 and `exchange_cutoff` == 5.0 and `M` == 1')
 label = r"$\Psi^{CPD} (M=1, K=5)$"
 error_wrt_dmrg = (sub_df['energy']-dmrg_energies_values)/n_atoms
 axs[1].plot(sub_df['distance'], error_wrt_dmrg, color="0.2", linestyle="--", label=label)
