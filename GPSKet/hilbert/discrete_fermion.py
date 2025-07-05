d8b05fc25ccbaec5ade0f7fe48ae248369c81082
Yannic Rath <yannic.rath@npl.co.uk>
2025-07-05 20:20:48 +0100

    Fix discrete Fermion Hilbert space for latest netket version

--- GPSKet/hilbert/discrete_fermion.py
+++ GPSKet/hilbert/discrete_fermion.py
@@ -1,55 +1,41 @@
 import jax.numpy as jnp
 import numpy as np
 from typing import Optional, Tuple
-from netket.hilbert import HomogeneousHilbert
+from netket.hilbert.custom_hilbert import HomogeneousHilbert
 from netket.hilbert.constraint import DiscreteHilbertConstraint
-from netket.hilbert.constraint import SumOnPartitionConstraint
-from netket.hilbert.index import optimalConstrainedHilbertindex
-from netket.hilbert.index import SumConstrainedHilbertIndex
-from netket.hilbert.index import SumOnPartitionConstrainedHilbertIndex
 from netket.utils import struct, StaticRange
 from netket.utils.types import Scalar, Array
+import jax
 
 
 class FermionicSumConstraint(DiscreteHilbertConstraint):
     size: int = struct.field(pytree_node=False)
     n_elec: Tuple[Scalar, Scalar] = struct.field(pytree_node=False)
-    sub_constraint: SumOnPartitionConstraint = struct.field(pytree_node=False)
 
     def __init__(self, size, n_elec):
         self.size = size
         self.n_elec = n_elec
-        self.sub_constraint = SumOnPartitionConstraint(
-            self.n_elec, (self.size, self.size)
-        )
 
+    @jax.jit
     def __call__(self, x: Array) -> Array:
         x_up = jnp.where(x == 3, 1, 0)
         x_up = jnp.where(x == 1, 1, x_up)
         x_dn = jnp.where(x == 3, 1, 0)
         x_dn = jnp.where(x == 2, 1, x_dn)
-        return self.sub_constraint(jnp.hstack((x_up, x_dn)))
-    
+
+        return jnp.logical_and(
+            jnp.sum(x_up, axis=-1) == self.n_elec[0],
+            jnp.sum(x_dn, axis=-1) == self.n_elec[1],
+        )
+
     def __hash__(self):
         return hash(("FermionicSumConstraint", self.n_elec))
-    
+
     def __eq__(self, other):
         if isinstance(other, FermionicSumConstraint):
             return self.size == other.size and self.n_elec == other.n_elec
         return False
-    
-@optimalConstrainedHilbertindex.dispatch
-def optimalConstrainedHilbertindex(
-    local_states, size, constraint: FermionicSumConstraint
-):
-    local_states = StaticRange(0, 1, 2, dtype=local_states.dtype)
-    index = SumOnPartitionConstrainedHilbertIndex(
-        sub_indices=tuple(
-            SumConstrainedHilbertIndex(local_states, size, sv)
-            for sv in constraint.n_elec
-        )
-    )
-    return index
+
 
 class FermionicDiscreteHilbert(HomogeneousHilbert):
     def __init__(self, N: int = 1, n_elec: Optional[Tuple[int, int]] = None):
@@ -85,7 +71,3 @@ class FermionicDiscreteHilbert(HomogeneousHilbert):
 
     def states_to_local_indices(self, x):
         return x.astype(jnp.uint8)
-    
-    def all_states(self) -> np.ndarray:
-        all_states = super().all_states()
-        return all_states[:, :self.size] + 2*all_states[:, self.size:]
